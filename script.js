915bb430ea6db8508cd5911ce6b51da8dde42a8f
Jacob N <177889821+baaramuser123@users.noreply.github.com>
2025-03-12 18:23:25 -0400

    -Initialize directory files for project -Display a 16 x 16 grid on webpage

---
+++ script.js
@@ -0,0 +1,9 @@
+{
+console.log("Hellow world!");
+const container = document.querySelector(".container");
+for (let i=0; i < 256; i++) {
+    const item = document.createElement("div");
+    item.classList.add("item");
+    container.appendChild(item);
+}
+};
