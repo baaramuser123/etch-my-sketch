e5f71e69bf6d7c758c588c023d65301e1222a785
Jacob N <177889821+baaramuser123@users.noreply.github.com>
2025-03-14 17:18:37 -0400

    Add erase button with full functionality

--- script.js
+++ script.js
@@ -1,9 +1,34 @@
 {
-console.log("Hellow world!");
 const container = document.querySelector(".container");
-for (let i=0; i < 256; i++) {
-    const item = document.createElement("div");
-    item.classList.add("item");
-    container.appendChild(item);
+const eraseButton = document.querySelector("button");
+let items = document.querySelectorAll(".item");
+createBoard();
+
+
+
+container.addEventListener("mouseout", (e) => {
+    if (e.target.classList.contains("item")) e.target.style.backgroundColor = "red";
+})
+
+eraseButton.addEventListener("click", eraseBoard);
+
+
+
+function createBoard () {
+    for (let i=0; i < 256; i++) {
+        const item = document.createElement("div");
+        item.classList.add("item");
+        container.appendChild(item);
+    }
+    items = document.querySelectorAll(".item");
 }
+
+function eraseBoard () {
+    items.forEach((element) => element.remove());
+    // container.childNodes.forEach((element) => element.remove());
+
+    createBoard();
+}
+
+
 };
