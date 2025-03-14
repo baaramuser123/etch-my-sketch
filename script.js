ee5c45f5f7c2f1cd990720ed3313434620854169
Jacob N <177889821+baaramuser123@users.noreply.github.com>
2025-03-14 18:06:41 -0400

    -Add button for resizing of board -Increase size of board

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 {
 const container = document.querySelector(".container");
-const eraseButton = document.querySelector("button");
+const eraseButton = document.querySelector("#erase");
+const qualityButton = document.querySelector('#quality');
 let items = document.querySelectorAll(".item");
+let boxesLength = 16;
+
 createBoard();
 
 
@@ -10,12 +13,24 @@ container.addEventListener("mouseout", (e) => {
     if (e.target.classList.contains("item")) e.target.style.backgroundColor = "red";
 })
 
+document.addEventListener("click", (event) => {
+    if (event.target.id == "erase"){
+        eraseBoard();
+    }
+    else if (event.target.id == "quality"){
+        boxesLength = prompt("Enter a number between 10 and 100");
+        eraseBoard();
+        let root = document.querySelector(':root');
+        root.style.setProperty('--rowCount', boxesLength);
+    }
+})
+
 eraseButton.addEventListener("click", eraseBoard);
 
 
 
 function createBoard () {
-    for (let i=0; i < 256; i++) {
+    for (let i=0; i < boxesLength ** 2; i++) {
         const item = document.createElement("div");
         item.classList.add("item");
         container.appendChild(item);
